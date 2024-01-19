b8528916d2ee37ac8a455212aa7d93e9d837f417
vulkz <gabrielcosta3536@gmail.com>
2024-01-18 22:46:36 -0300

    iniciando o consumo da api

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+const offset = 0;
+const limit = 10;
+
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+fetch(url)
+    .then((response) => {return response.json()})
+    .then((jsonBody) => {console.log(jsonBody)})
+    .catch((error) => {console.error(error)})
